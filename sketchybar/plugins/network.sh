23eaa1cf833dc2759f5a2abb44c1451943791732
Jayson Acosta <jaxoco@amazon.com>
2026-04-27 11:47:33 -0700

    improve network script

--- sketchybar/plugins/network.sh
+++ sketchybar/plugins/network.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-SSID="$(ipconfig getsummary en0 | grep -Eo "^\s*SSID\s*:(\s.+)$" | cut -f2 -d ":" | xargs)"
+for iface in $(networksetup -listallhardwareports | awk '/Hardware Port:/ && !/Wi-Fi/{getline; print $2}'); do
+    if ipconfig getifaddr "$iface" &>/dev/null; then
+        sketchybar --set "$NAME" icon="􀤆" label="$iface"
+        exit 0
+    fi
+done
 
-if [[ -z "$SSID" ]]; then
-    ICON="􁣡"
-    else
-    ICON="􀤆"
+SSID="$(ipconfig getsummary en0 | awk -F: '/^[[:space:]]*SSID/{print $2}' | xargs)"
+
+if [[ -n "$SSID" ]]; then
+    sketchybar --set "$NAME" icon="􀙇" label="$SSID"
+else
+    sketchybar --set "$NAME" icon="􁣡" label="-"
 fi
-
-sketchybar --set "$NAME" icon="${ICON}" label="${SSID:--}"
